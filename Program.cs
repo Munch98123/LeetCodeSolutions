c36f9d8165a4ea6ba49ee9644e7f26607eaf2758
Daniel Munchbach <daniel.munchbach981@gmail.com>
2024-07-25 20:09:34 -0400

    Clean-up

--- Program.cs
+++ Program.cs
@@ -22,6 +22,7 @@ namespace LeetCodeProblems
             Console.WriteLine("--Buy and Sell Stocks--");
             Console.WriteLine($"Max Profit: {BuyAndSellStock.GetMaxProfit(new int[6] { 7, 1, 5, 3, 6, 4 })}");
 
+            //Array Contains Duplicates
             Console.WriteLine($"--Contains Duplicates--");
             Console.WriteLine("[1, 2, 3, 1]");
             Console.WriteLine(ContainsDuplicate217.ContainsDuplicate(new int[4] { 1, 2, 3, 1 }));
@@ -29,14 +30,17 @@ namespace LeetCodeProblems
             Console.WriteLine("[1,1,1,3,3,4,3,2,4,2]");
             Console.WriteLine(ContainsDuplicate217.ContainsDuplicate(new int[10] { 1, 1, 1, 3, 3, 4, 3, 2, 4, 2 }));
 
+            //Product of elements in an array except the current element
             Console.WriteLine($"--Product Except Self--");
             Console.WriteLine("[1, 2, 3, 4]");
             Array.ForEach(ProductOfArray.ProductExceptSelf(new int[6] { 1, 2, 3, 4, 5, 6 }), print);
 
+            //Maximum value from subarray 
             Console.WriteLine($"--Maximum Subarray--");
             Console.WriteLine($"[-2, 1, -3, 4, -1, 2, 1, -5, 4]");
             Console.WriteLine(MaximumSubarray53.MaxSubarrayKadanes(new int[] { -2, 1, -3, 4, -1, 2, 1, -5, 4 }));
 
+            //Minimum value in pre-sorted, rotated x number of times array
             Console.WriteLine($"--Minimum in Sorted Array--");
             Console.WriteLine($"[ 4, 5, 6, 7, 0, 1, 2 ]");
             Console.WriteLine(MinimumInSortedArray153.FindMinBinary(new int[] { 4, 5, 6, 7, 0, 1, 2 }));
