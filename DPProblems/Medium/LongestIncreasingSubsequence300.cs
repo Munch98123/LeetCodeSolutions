371f34756c9a4efb76de7813d1a492974353f58a
Daniel Munchbach <daniel.munchbach981@gmail.com>
2024-07-28 22:45:44 -0400

    Added more problems (104, 300)

---
+++ DPProblems/Medium/LongestIncreasingSubsequence300.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.DPProblems.Medium
+{
+    public class LongestIncreasingSubsequence300
+    {
+        public static int GetLongestSubsequence(int[] nums)
+        {
+            List<int> currentSequence = new List<int>();
+            List<int> longestSequence = new List<int>();
+
+            currentSequence.Add(nums[0]);
+
+            for(int i = 1; i < nums.Length; i++)
+            {
+                if (nums[i] > nums[i - 1])
+                {
+                    currentSequence.Add(nums[i]);
+                }
+                else if (nums[i] < nums[i - 1])
+                {
+
+                }
+            }
+
+            return int.MaxValue;
+        }
+    }
+}
---
+++ TreeProblems/Easy/MaximumDepthOfBinaryTree104.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LeetCodeProblems.TreeProblems.Easy
+{
+    public class MaximumDepthOfBinaryTree104
+    {
+        public static int GetMaxDepth(TreeNode currentNode)
+        {
+            int leftDepth = 0, rightDepth = 0;
+            if (currentNode == null)
+            {
+                return 0;
+            }
+            if(currentNode.left != null) 
+            {
+                leftDepth = GetMaxDepth(currentNode.left);
+            }
+            if(currentNode.right != null)
+            {
+                rightDepth = GetMaxDepth(currentNode.right);
+            }
+            
+            return Math.Max(leftDepth, rightDepth) + 1;
+        }
+    }
+}
